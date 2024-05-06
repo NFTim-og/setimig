00ea9ab5a1ff0fd43ffc85ed9f84a493caa27626
Tim <timgomperts@gmail.com>
2024-05-06 10:11:42 +0200

    Tot funciona correctament

--- game.js
+++ game.js
@@ -7,6 +7,8 @@ class Game {
         this.player = new Player("Player");
         this.computer = new Computer("Computer");
         this.deck = new Deck();
+        this.playerScore = 0;
+        this.computerScore = 0;
     }
 
     initGame() {
@@ -29,6 +31,9 @@ class Game {
         this.computer.calculatePoints();
         computerCardImg.classList.add('firstCardHidden');
         document.getElementById("computer-cards").append(computerCardImg);
+
+        document.getElementById("player-score").innerText = this.playerScore;
+        document.getElementById("computer-score").innerText = this.computerScore;
     }
 
     dealCard(player, numCards) {
@@ -53,6 +58,8 @@ class Game {
         if (this.player.points > 7.5) {
             let message = "T'has passat!";
             document.getElementById("results").innerText = message;
+            this.computerScore++;
+            document.getElementById("computer-score").innerText = this.computerScore;
         }
 
         document.getElementById("player-sum").innerText = this.player.points;
@@ -64,7 +71,8 @@ class Game {
         or equal to the player's points or until they reach 7.5*/
         while (this.computer.points < this.player.points && this.computer.points < 7.5) {
             let card = this.deck.pop();
-            this.computer.points += this.computer.calculatePoints();
+            this.computer.cards.push(card);
+            this.computer.calculatePoints();
             let computerCardImg = document.createElement("img");
             computerCardImg.src = "./baralla/" + card + ".jpg";
             document.getElementById("computer-cards").append(computerCardImg);
@@ -75,6 +83,7 @@ class Game {
         let computerCards = document.getElementById("computer-cards");
         let firstCard = computerCards.getElementsByClassName("firstCardHidden")[0];
         firstCard.classList.remove("firstCardHidden");
+        document.getElementById("hidden").remove();
 
         document.getElementById("computer-sum").innerText = this.computer.points;
 
@@ -84,25 +93,65 @@ class Game {
     
         if (this.player.points > 7.5) {
             message = "Has perdut...";
+            // If the player has more than 7.5 points, the computer wins
+            // If the computer has more than 7.5 points, the player wins
+            computerScore++;
         } else {
             if (this.player.points > 7.5 && this.player.points == this.computer.points && this.computer.points > this.player.points) {
                 message = "Has perdut...";
+                this.computerScore++;
             } else if (this.computer.points > 7.5 && this.player.points <= 7.5) {
                 message = "Has guanyat!";
+                this.playerScore++;
             } else if (this.player.points <= 7.5 && this.computer.points <= 7.5) {
                 if (this.player.points > this.computer.points) {
                     message = "Has guanyat!";
-               } else if (this.player.points < this.computer.points) {
+                    this.playerScore++;
+                } else if (this.player.points < this.computer.points) {
                     message = "Has perdut...";
+                    this.computerScore++;
                 } else {
                     message = "Has perdut..."; // If both have the same points, computer wins
+                    this.computerScore++;
                 }
             }
         }
 
         document.getElementById("results").innerText = message;
+
+        document.getElementById("player-score").innerText = this.playerScore;
+        document.getElementById("computer-score").innerText = this.computerScore;
     
     }
+
+    reset() {
+        // Reset player and computer hands
+        this.player.cards = [];
+        this.player.points = 0;
+        this.computer.cards = [];
+        this.computer.points = 0;
+    
+        // Reset deck
+        this.deck = new Deck();
+        this.deck.shuffle();
+    
+        // Reset UI elements
+        document.getElementById("player-cards").innerHTML = "";
+        document.getElementById("computer-cards").innerHTML = "";
+        document.getElementById("results").innerText = "";
+        document.getElementById("player-sum").innerText = "0";
+        document.getElementById("computer-sum").innerText = "0";
+    
+        // Call initGame to deal initial cards again
+        this.initGame();
+
+        //Add back the hidden card for the computer
+        let computerHiddenCard = document.createElement("img");
+        computerHiddenCard.src = "./baralla/back.png";
+        computerHiddenCard.id = "hidden";
+        document.getElementById("computer-cards").appendChild(computerHiddenCard);
+    }
+    
 }
 
 export default Game;
